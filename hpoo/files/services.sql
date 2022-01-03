73516fc8288d8ccac7849400d71d0c2b943e2e2a
Erwin <eveliz@outlook.com>
2022-01-03 14:30:05 -0500

    output

--- hpoo/files/services.sql
+++ hpoo/files/services.sql
@@ -105,14 +105,14 @@ END
 
 /* -------------------------------------------------------------------------------------------------------------*/
 
--- SELECT  ServiceName AS 'ServiceName'
---    ,ServiceStatus AS 'ServiceStatus'
---    ,StatusDateTime AS 'StatusDateTime'
---    ,ServerName 
---    ,PhysicalSrverName
--- FROM #ServiceStatus
+SELECT  ServiceName AS 'ServiceName'
+   ,ServiceStatus AS 'ServiceStatus'
+   ,StatusDateTime AS 'StatusDateTime'
+   ,ServerName 
+   ,PhysicalSrverName
+FROM #ServiceStatus
 
-select CONCAT(ServiceName, ';' , ServiceStatus, ';' , StatusDateTime, ';' , ServerName, ';' , PhysicalSrverName) as RSLT from #ServiceStatus
+-- select CONCAT(ServiceName, ';' , ServiceStatus, ';' , StatusDateTime, ';' , ServerName, '; ' , PhysicalSrverName) as RSLT from #ServiceStatus
 /*Perform cleanup*/
 
 -- INSERT INTO ServiceStatus(ServiceName, ServiceStatus, StatusDateTime, ServerName, PhysicalSrverName)
